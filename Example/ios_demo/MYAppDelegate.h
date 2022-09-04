766aa1b4a41daf0ef77063756d12bab0951cc64c
CongL.Hu <1580682761@qq.com>
2022-09-04 10:21:52 +0800

    create

---
+++ Example/ios_demo/MYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MYAppDelegate.h
+//  ios_demo
+//
+//  Created by CongL.Hu on 09/04/2022.
+//  Copyright (c) 2022 CongL.Hu. All rights reserved.
+//
+
+@import UIKit;
+
+@interface MYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
